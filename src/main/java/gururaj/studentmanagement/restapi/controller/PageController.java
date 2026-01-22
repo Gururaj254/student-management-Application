71b8416207dc02a56a234edaf6a4699ae2ae87fd
Gururaj254 <gururajdharmashetti390@gmail.com>
2026-01-22 11:59:42 +0530

    new page

--- src/main/java/gururaj/studentmanagement/restapi/controller/PageController.java
+++ src/main/java/gururaj/studentmanagement/restapi/controller/PageController.java
@@ -1,4 +1,4 @@
-package gururaj.studentmanagement.restapi.Controller;
+package gururaj.studentmanagement.restapi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
